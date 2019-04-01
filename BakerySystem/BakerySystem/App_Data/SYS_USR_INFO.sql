47a5fd88b758cae251b28b9eef9327574e689609
buddydevbigb <n.usm777@gmail.com>
2019-04-01 13:05:17 +0500

    db added
    DB Added.

---
+++ BakerySystem/BakerySystem/App_Data/SYS_USR_INFO.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].SYS_USR_INFO
+(
+	[UserId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserName] NVARCHAR(50) NULL, 
+    [UserPassowrd] NCHAR(10) NULL, 
+    [MachineIP] NVARCHAR(50) NULL, 
+    [OperatingSystem] NVARCHAR(50) NULL, 
+    [LoginTime] DATETIME NULL
+)
+
+GO
+
