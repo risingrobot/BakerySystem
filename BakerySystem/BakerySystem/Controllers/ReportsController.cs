ea07c83930ff34ef38e5699e004f9f2eac59c5b8
BUDYDEV <arsalan.usmani94@gmail.com>
2019-04-09 23:51:27 +0800

    adding reports backend

--- BakerySystem/BakerySystem/Controllers/ReportsController.cs
+++ BakerySystem/BakerySystem/Controllers/ReportsController.cs
@@ -17,14 +17,37 @@ namespace BakerySystem.Controllers
         [HttpGet]
         public ActionResult GetReport()
         {
+            string reportType = "";
+            
             using (BKRY_MNGT_SYSEntities db = new BKRY_MNGT_SYSEntities())
             {
-                List<BKRY_ITEMS> bkryList = db.BKRY_ITEMS.ToList<BKRY_ITEMS>();
-                foreach (BKRY_ITEMS item in bkryList)
+                switch (reportType)
                 {
-                    item.image = null;
+                    case "1":
+                        List<LoginDetail> LoginDetailList = db.LoginDetails.ToList<LoginDetail>();
+                        LoginDetailList = LoginDetailList.Where(x=>x.LoginTime >= DateTime.Now && x.LoginTime <= DateTime.Now ).ToList();
+                        return Json(new { data = LoginDetailList }, JsonRequestBehavior.AllowGet);                       
+                    case "2":
+                        List<BKRY_ORDER> BKRY_ORDERList = db.BKRY_ORDER.ToList<BKRY_ORDER>();
+                        BKRY_ORDERList = BKRY_ORDERList.Where(x => x.inserted_dt >= DateTime.Now && x.inserted_dt <= DateTime.Now).ToList();
+                        return Json(new { data = BKRY_ORDERList }, JsonRequestBehavior.AllowGet);
+                       
+                    case "3":
+                        List<BKRY_DELIVERY> BKRY_DELIVERYList = db.BKRY_DELIVERY.ToList<BKRY_DELIVERY>();
+                        BKRY_DELIVERYList = BKRY_DELIVERYList.Where(x => x.inserted_dt >= DateTime.Now && x.inserted_dt <= DateTime.Now).ToList();
+                        return Json(new { data = BKRY_DELIVERYList }, JsonRequestBehavior.AllowGet);                       
+                    case "4":
+                        List<BKRY_ITEMS> bkryList = db.BKRY_ITEMS.ToList<BKRY_ITEMS>();
+                        bkryList = bkryList.Where(x => x.add_date >= DateTime.Now && x.add_date <= DateTime.Now).ToList();
+                        foreach (BKRY_ITEMS item in bkryList)
+                        {
+                            item.image = null;
+                        }
+                        return Json(new { data = bkryList }, JsonRequestBehavior.AllowGet);
+                    default:
+                        break;
                 }
-                return Json(new { data = bkryList }, JsonRequestBehavior.AllowGet);
+                return null;
             }
         }
 
