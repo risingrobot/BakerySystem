c5474a35960a3e4344b165524138b5d0f2b5b80b
BUDYDEV <arsalan.usmani94@gmail.com>
2019-04-11 20:02:48 +0800

    fixing reports

--- BakerySystem/BakerySystem/Controllers/ReportsController.cs
+++ BakerySystem/BakerySystem/Controllers/ReportsController.cs
@@ -15,30 +15,29 @@ namespace BakerySystem.Controllers
             return View();
         }
         [HttpGet]
-        public ActionResult GetReport()
+        public ActionResult GetReport(string nm_rpttype,string nm_dtfrom,string nm_dtTo)
         {
-            string reportType = "";
-            
+                        
             using (BKRY_MNGT_SYSEntities db = new BKRY_MNGT_SYSEntities())
             {
-                switch (reportType)
+                switch (nm_rpttype)
                 {
                     case "1":
                         List<LoginDetail> LoginDetailList = db.LoginDetails.ToList<LoginDetail>();
-                        LoginDetailList = LoginDetailList.Where(x=>x.LoginTime >= DateTime.Now && x.LoginTime <= DateTime.Now ).ToList();
+                        LoginDetailList = LoginDetailList.Where(x=>x.LoginTime >= (nm_dtfrom != "" ? Convert.ToDateTime(nm_dtfrom) : DateTime.Now) && x.LoginTime <= (nm_dtTo != "" ? Convert.ToDateTime(nm_dtTo) : DateTime.Now)).ToList();
                         return Json(new { data = LoginDetailList }, JsonRequestBehavior.AllowGet);                       
                     case "2":
                         List<BKRY_ORDER> BKRY_ORDERList = db.BKRY_ORDER.ToList<BKRY_ORDER>();
-                        BKRY_ORDERList = BKRY_ORDERList.Where(x => x.inserted_dt >= DateTime.Now && x.inserted_dt <= DateTime.Now).ToList();
+                        BKRY_ORDERList = BKRY_ORDERList.Where(x => x.inserted_dt >= (nm_dtfrom != "" ? Convert.ToDateTime(nm_dtfrom) : DateTime.Now) && x.inserted_dt <= (nm_dtTo != "" ? Convert.ToDateTime(nm_dtTo) : DateTime.Now)).ToList();
                         return Json(new { data = BKRY_ORDERList }, JsonRequestBehavior.AllowGet);
                        
                     case "3":
                         List<BKRY_DELIVERY> BKRY_DELIVERYList = db.BKRY_DELIVERY.ToList<BKRY_DELIVERY>();
-                        BKRY_DELIVERYList = BKRY_DELIVERYList.Where(x => x.inserted_dt >= DateTime.Now && x.inserted_dt <= DateTime.Now).ToList();
+                        BKRY_DELIVERYList = BKRY_DELIVERYList.Where(x => x.inserted_dt >= (nm_dtfrom != "" ? Convert.ToDateTime(nm_dtfrom) : DateTime.Now) && x.inserted_dt <= (nm_dtTo != "" ? Convert.ToDateTime(nm_dtTo) : DateTime.Now)).ToList();
                         return Json(new { data = BKRY_DELIVERYList }, JsonRequestBehavior.AllowGet);                       
                     case "4":
                         List<BKRY_ITEMS> bkryList = db.BKRY_ITEMS.ToList<BKRY_ITEMS>();
-                        bkryList = bkryList.Where(x => x.add_date >= DateTime.Now && x.add_date <= DateTime.Now).ToList();
+                        bkryList = bkryList.Where(x => x.add_date >= (nm_dtfrom != "" ? Convert.ToDateTime(nm_dtfrom) : DateTime.Now) && x.add_date <= (nm_dtTo != "" ? Convert.ToDateTime(nm_dtTo) : DateTime.Now)).ToList();
                         foreach (BKRY_ITEMS item in bkryList)
                         {
                             item.image = null;
