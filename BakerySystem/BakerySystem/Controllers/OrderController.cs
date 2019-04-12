1191df4021fbe4ce3b818d059e2eaa801fc53020
BUDYDEV <arsalan.usmani94@gmail.com>
2019-04-13 00:55:02 +0800

    touching

--- BakerySystem/BakerySystem/Controllers/OrderController.cs
+++ BakerySystem/BakerySystem/Controllers/OrderController.cs
@@ -19,16 +19,27 @@ namespace BakerySystem.Controllers
         {
             List<GetOrderDetail> bkryList = new List<GetOrderDetail>();
             List<BKRY_ITEMS> lstBKRY_ITEMS = new List<BKRY_ITEMS>();
-
+            List<GetOrderDetail2> bkrList2 = new List<GetOrderDetail2>();
             using (BKRY_MNGT_SYSEntities db = new BKRY_MNGT_SYSEntities())
             {
                 bkryList = db.GetOrderDetails.Where(x => x.OrderId == id).ToList<GetOrderDetail>();
                 foreach (var item in bkryList)
                 {
                     item.OrderDetails = null;
-                    item.address = item.address + " " + item.street + " " + item.postCode; 
+                    item.address = item.address + " " + item.street + " " + item.postCode;
+                    GetOrderDetail2 st = new GetOrderDetail2
+                    {
+                        add_dtee = Convert.ToDateTime(item.Orderon).ToLongDateString(),
+                        OrderId = item.OrderId,
+                        Orderon = item.Orderon,
+                        personname = item.personname,
+                        email  =item.email,
+                        address = item.address,
+                        Delivered = item.Delivered
+    };
+                    bkrList2.Add(st);
                 }
-                return Json(new { data = bkryList }, JsonRequestBehavior.AllowGet);
+                return Json(new { data = bkrList2 }, JsonRequestBehavior.AllowGet);
             }
         }
         [HttpPost]
